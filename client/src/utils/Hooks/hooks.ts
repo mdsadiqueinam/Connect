821da47ca2275fb5bcf9ee7ff08cb06602db3d84
mdsadiqueinam <md.sadique32@gmail.com>
2021-09-30 11:14:15 +0530

    implemented check username/email query

--- client/src/utils/Hooks/hooks.ts
+++ client/src/utils/Hooks/hooks.ts
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useQuery, gql } from "@apollo/client";
+import handleErrors from "config/handleErrors";
 
 export const useForm = (callback: Function, initialState: any = {}) => {
   const [values, setValues] = useState(initialState);
@@ -19,3 +21,28 @@ export const useForm = (callback: Function, initialState: any = {}) => {
   };
 };
 
+const CHECK_USERNAME = gql`
+  query checkUsername($username: String!) {
+    checkUsername(username: $username)
+  }
+`;
+
+const CHECK_EMAIL = gql`
+  query checkEmail($email: String!) {
+    checkEmail(email: $email)
+  }
+`;
+
+export function useCheckUsername(options: object, errorLocation: string) {
+  return useQuery(CHECK_USERNAME, {
+    ...options,
+    onError: (error) => handleErrors(error, errorLocation),
+  })
+}
+
+export function useCheckEmail(options: object, errorLocation: string) {
+  return useQuery(CHECK_EMAIL, {
+    ...options,
+    onError: (error) => handleErrors(error, errorLocation),
+  })
+}
